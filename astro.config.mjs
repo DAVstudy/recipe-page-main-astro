1729300bdb1e9dd39b4a03858ac6f9a7ee95d8bc
DIEGO ARENAS <arenasvega.diego@gmail.com>
2025-04-18 18:54:46 -0400

    Corrige la ruta de importación de global.css en Layout.astro y asegura la configuración del sitio en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://DAVstudy.github.io',
+  base: '/recipe-page-main-astro/',
   vite: {
     plugins: [tailwindcss()]
   }
